91e94a3bfb4c2b97924f3b5b792166c76163812f
Kaushal Chandrapal <kc.kaushalchandrapal@gmail.com>
2023-05-20 04:11:27 +0530

    ashj (#4)
    Co-authored-by: Kaushal Chandrapal <kaushal.chandrapal@dltapps.co.uk>

---
+++ src/MyComponentDemo.ts
@@ -0,0 +1,1 @@
+export { default as TextFieldPage } from './Pages/TextFieldPage/textFieldPage';
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [reactRefresh()],
+  build: {
+    lib: {
+      entry: 'src/MyComponentDemo.ts',
+      name: 'MyComponentDemo',
+      fileName: (format) => `my-component-demo.${format}.ts`,
+    },
+    rollupOptions: {
+      external: ['react'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
+    },
+  },
 })
