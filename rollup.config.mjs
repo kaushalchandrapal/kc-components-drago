1195055ce44195ab68bef330d2cb07df7281607d
Kaushal Chandrapal <kc.kaushalchandrapal@gmail.com>
2023-05-21 03:44:12 +0530

    Changes added

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -25,7 +25,7 @@ export default [
         entries: [
           {
             find: 'TextFieldPage',
-            replacement: './src/TextFieldPage'
+            replacement: './src'
           }
         ]
       }),
